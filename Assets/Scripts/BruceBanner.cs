44906f40adf701506090847e9288da9092bbda53
mcdona1228 <61477480+mcdona1228@users.noreply.github.com>
2022-03-28 09:31:06 -0700

    Assets

---
+++ Assets/Scripts/BruceBanner.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BruceBanner : MonoBehaviour
+{
+    public Door theDoor;
+    public GameObject theTreasure;
+    public GameObject test;
+    bool executingBehavior = false;
+    Task myCurrentTask;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (!executingBehavior)
+            {
+                executingBehavior = true;
+                myCurrentTask = BuildTask_GetTreasue();
+
+                EventBus.StartListening(myCurrentTask.TaskFinished, OnTaskFinished);
+                myCurrentTask.run();
+            }
+        }
+    }
+
+    void OnTaskFinished()
+    {
+        EventBus.StopListening(myCurrentTask.TaskFinished, OnTaskFinished);
+        //Debug.Log("Behavior complete! Success = " + myCurrentTask.succeeded);
+        executingBehavior = false;
+    }
+    
+    Task BuildTask_GetTreasue()
+    {
+        // create our behavior tree based on Millington pg. 344
+        // building from the bottom up
+        List<Task> taskList = new List<Task>();
+
+        // if door isn't locked, open it
+        Task isDoorNotLocked = new IsFalse(theDoor.isLocked);
+        Task waitABeat = new Wait(0.5f);
+        Task openDoor = new OpenDoor(theDoor);
+        taskList.Add(isDoorNotLocked);
+        taskList.Add(waitABeat);
+        taskList.Add(openDoor);
+        Sequence openUnlockedDoor = new Sequence(taskList);
+
+        // barge a closed door
+        taskList = new List<Task>();
+        Task isDoorClosed = new IsTrue(theDoor.isClosed);
+        Task hulkOut = new HulkOut(this.gameObject);
+        Task bargeDoor = new BargeDoor(theDoor.transform.GetChild(0).GetComponent<Rigidbody>());
+        Task isDoorBargable = new IsFalse(theDoor.isSteel);
+        Task isDoorThrowable = new IsFalse(theDoor.isTitanium);
+        Task isDoorUnbreakable = new IsFalse(theDoor.isVibranium);
+        taskList.Add(isDoorClosed);
+        taskList.Add(waitABeat);
+        taskList.Add(hulkOut);
+        taskList.Add(waitABeat);
+        taskList.Add(isDoorBargable);
+        taskList.Add(waitABeat);
+        taskList.Add(bargeDoor);
+        Sequence bargeClosedDoor = new Sequence(taskList);
+
+        // Squish steel door
+        
+        taskList = new List<Task>();
+        Task isDoorSteel = new IsTrue(theDoor.isSteel);
+        //Task hulkOut = new HulkOut(this.gameObject);
+        Task squishDoor = new SquishDoor(theDoor.transform.GetChild(0).GetComponent<Rigidbody>());
+        taskList.Add(isDoorSteel);
+        taskList.Add(waitABeat);
+        taskList.Add(hulkOut);
+        taskList.Add(waitABeat);
+        taskList.Add(isDoorBargable);
+        taskList.Add(waitABeat);
+        taskList.Add(squishDoor);
+        Sequence squishSteelDoor = new Sequence(taskList);
+
+        // Titanium door brings hulk into a rage
+        taskList = new List<Task>();
+        Task isDoorTitanium = new IsTrue(theDoor.isTitanium);
+        Task hulkRage = new HulkRage(this.gameObject);
+        Task titaniumDoor = new BargeDoor(theDoor.transform.GetChild(0).GetComponent<Rigidbody>());
+        taskList.Add(isDoorTitanium);
+        taskList.Add(waitABeat);
+        taskList.Add(hulkOut);
+        taskList.Add(waitABeat);
+        taskList.Add(hulkRage);
+        taskList.Add(waitABeat);
+        taskList.Add(titaniumDoor);
+        Sequence throwTitaniumDoor = new Sequence(taskList);
+
+        //Giving up
+        taskList = new List<Task>();
+        Task isDoorVibranium = new IsTrue(theDoor.isVibranium);
+        Task giveUp = new GiveUp(this.gameObject);
+        taskList.Add(isDoorVibranium);
+        taskList.Add(waitABeat);
+        taskList.Add(hulkOut);
+        taskList.Add(waitABeat);
+        taskList.Add(hulkRage);
+        taskList.Add(waitABeat);
+        taskList.Add(giveUp);
+        Sequence hulkgivesUp = new Sequence(taskList);
+
+        // open a closed door, one way or another
+        taskList = new List<Task>();
+        taskList.Add(openUnlockedDoor);
+        taskList.Add(bargeClosedDoor);
+        taskList.Add(squishSteelDoor);
+        taskList.Add(throwTitaniumDoor);
+        taskList.Add(hulkgivesUp);
+        Selector openTheDoor = new Selector(taskList);
+
+        // get the treasure when the door is closed
+        taskList = new List<Task>();
+        Task moveToDoor = new MoveKinematicToObject(this.GetComponent<Kinematic>(), theDoor.gameObject);
+        Task moveToTreasure = new MoveKinematicToObject(this.GetComponent<Kinematic>(), theTreasure.gameObject);
+        taskList.Add(moveToDoor);
+        taskList.Add(waitABeat);
+        taskList.Add(openTheDoor); // one way or another
+        taskList.Add(waitABeat);
+        taskList.Add(moveToTreasure);
+        Sequence getTreasureBehindClosedDoor = new Sequence(taskList);
+
+        // get the treasure when the door is open 
+        taskList = new List<Task>();
+        Task isDoorOpen = new IsFalse(theDoor.isClosed);
+        taskList.Add(isDoorOpen);
+        taskList.Add(moveToTreasure);
+        Sequence getTreasureBehindOpenDoor = new Sequence(taskList);
+
+        // get the treasure, one way or another
+        taskList = new List<Task>();
+        taskList.Add(getTreasureBehindOpenDoor);
+        taskList.Add(getTreasureBehindClosedDoor);
+        Selector getTreasure = new Selector(taskList);
+
+        return getTreasure;
+    }
+}
---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool isClosed = false;
+    public bool isLocked = false;
+    public bool isSteel = false;
+    public bool isTitanium = false;
+    public bool isVibranium = false;
+
+    Vector3 closedRotation = new Vector3(0, 0, 0);
+    Vector3 openRotation = new Vector3(0, -135, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isClosed)
+        {
+            transform.eulerAngles = closedRotation;
+        }
+        else
+        {
+            transform.eulerAngles = openRotation;
+        }
+        if (isSteel)
+        {
+            //transform.localScale /= 2;
+        }
+    }
+
+    public bool Open()
+    {
+        if (isClosed && !isLocked)
+        {
+            //Debug.Log("door is now open");
+            isClosed = false;
+            transform.eulerAngles = openRotation;
+            return true;
+        }
+
+        //Debug.Log("door was either locked or already open");
+        return false;
+    }
+
+    public bool Close()
+    {
+        if (!isClosed)
+        {
+            //Debug.Log("door is now closed");
+            transform.eulerAngles = closedRotation;
+            isClosed = true;
+        }
+        return true;
+    }
+
+    public bool Steel()
+    {
+        if (isSteel && isClosed && isLocked)
+        {
+            Debug.Log("door is now squished");
+            transform.localScale /= 2;
+            isSteel = true;
+        }
+        return true;
+    }
+    public bool Titanium()
+    {
+        if (isTitanium && isClosed && isLocked)
+        {
+            Debug.Log("door is now titanium");
+            transform.eulerAngles = closedRotation;
+            isTitanium = true;
+        }
+        return true;
+    }
+    public bool Vibranium()
+    {
+        if (isVibranium && isClosed && isLocked)
+        {
+            Debug.Log("door is now vibranium");
+            transform.eulerAngles = closedRotation;
+            isVibranium = true;
+        }
+        return true;
+    }
+}
